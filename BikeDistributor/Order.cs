4d2c616ed45f7763649d0c28761859fd541d5fa2
jeremy zeidner <jeremy_zeidner@yahoo.com>
2014-10-19 11:55:59 -0400

    refactored order
    refactored order

--- BikeDistributor/Order.cs
+++ BikeDistributor/Order.cs
@@ -1,108 +1,61 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.Generic;
+using BikeDistributor.ReceiptRenderers;
 
 namespace BikeDistributor
 {
     public class Order
     {
-        private const double TaxRate = .0725d;
-        private readonly IList<Line> lines = new List<Line>();
+        private readonly IList<Line> _lines = new List<Line>();
 
-        public Order(string company)
+        public IList<OrderLineItem> LineItems;
+
+        public Order(Company company, double taxRate, IList<DiscountRule> discountRules,
+            IReceiptRenderer receiptRenderer)
         {
             Company = company;
+            LineItems = new List<OrderLineItem>();
+            TaxRate = taxRate;
+            ReceiptRenderer = receiptRenderer;
+            DiscountRules = discountRules;
         }
 
-        public string Company { get; private set; }
+        public IReceiptRenderer ReceiptRenderer { get; set; }
+        public double TaxRate { get; set; }
+        public double? OrderTotal { get; set; }
+        public double? TotalTax { get; set; }
+        public IList<DiscountRule> DiscountRules { get; set; }
+
+        public Company Company { get; private set; }
+
 
         public void AddRental(Line rental)
         {
-            lines.Add(rental);
+            _lines.Add(rental);
+        }
+         
+        public double CalculateTotal()
+        {
+            double Total = 0;
+            foreach (Line line in _lines)
+            {
+                double thisAmount = 0d;
+                var lineItem = new OrderLineItem();
+                lineItem.Bike = line.Bike;
+                lineItem.Quantity = line.Quantity;
+                lineItem.Calculate(DiscountRules);
+                LineItems.Add(lineItem);
+                Total += lineItem.Total.Value;
+            }
+            return Total;
         }
 
-        public string Receipt()
+        public string RenderReceipt()
         {
-            var totalAmount = 0d;
-            var result = new StringBuilder(string.Format("Order Receipt for {0}{1}", Company, Environment.NewLine));
-            foreach (var line in lines)
-            {
-                var thisAmount = 0d;
-                switch (line.Bike.Price)
-                {
-                    case Bike.OneThousand:
-                        if (line.Quantity >= 20)
-                            thisAmount += line.Quantity * line.Bike.Price * .9d;
-                        else
-                            thisAmount += line.Quantity * line.Bike.Price;
-                        break;
-                    case Bike.TwoThousand:
-                        if (line.Quantity >= 10)
-                            thisAmount += line.Quantity * line.Bike.Price * .8d;
-                        else
-                            thisAmount += line.Quantity * line.Bike.Price;
-                        break;
-                    case Bike.FiveThousand:
-                        if (line.Quantity >= 5)
-                            thisAmount += line.Quantity * line.Bike.Price * .8d;
-                        else
-                            thisAmount += line.Quantity * line.Bike.Price;
-                        break;
-                }
-                result.AppendLine(string.Format("\t{0} x {1} {2} = {3}", line.Quantity, line.Bike.Brand, line.Bike.Model, thisAmount.ToString("C")));
-                totalAmount += thisAmount;
-            }
-            result.AppendLine(string.Format("Sub-Total: {0}", totalAmount.ToString("C")));
-            var tax = totalAmount * TaxRate;
-            result.AppendLine(string.Format("Tax: {0}", tax.ToString("C")));
-            result.Append(string.Format("Total: {0}", (totalAmount + tax).ToString("C")));
-            return result.ToString();
+            OrderTotal = CalculateTotal();
+            TotalTax = OrderTotal.Value * TaxRate;
+            return ReceiptRenderer.Render(this);
         }
 
-        public string HtmlReceipt()
-        {
-            var totalAmount = 0d;
-            var result = new StringBuilder(string.Format("<html><body><h1>Order Receipt for {0}</h1>", Company));
-            if (lines.Any())
-            {
-                result.Append("<ul>");
-                foreach (var line in lines)
-                {
-                    var thisAmount = 0d;
-                    switch (line.Bike.Price)
-                    {
-                        case Bike.OneThousand:
-                            if (line.Quantity >= 20)
-                                thisAmount += line.Quantity*line.Bike.Price*.9d;
-                            else
-                                thisAmount += line.Quantity*line.Bike.Price;
-                            break;
-                        case Bike.TwoThousand:
-                            if (line.Quantity >= 10)
-                                thisAmount += line.Quantity*line.Bike.Price*.8d;
-                            else
-                                thisAmount += line.Quantity*line.Bike.Price;
-                            break;
-                        case Bike.FiveThousand:
-                            if (line.Quantity >= 5)
-                                thisAmount += line.Quantity*line.Bike.Price*.8d;
-                            else
-                                thisAmount += line.Quantity*line.Bike.Price;
-                            break;
-                    }
-                    result.Append(string.Format("<li>{0} x {1} {2} = {3}</li>", line.Quantity, line.Bike.Brand, line.Bike.Model, thisAmount.ToString("C")));
-                    totalAmount += thisAmount;
-                }
-                result.Append("</ul>");
-            }
-            result.Append(string.Format("<h3>Sub-Total: {0}</h3>", totalAmount.ToString("C")));
-            var tax = totalAmount * TaxRate;
-            result.Append(string.Format("<h3>Tax: {0}</h3>", tax.ToString("C")));
-            result.Append(string.Format("<h2>Total: {0}</h2>", (totalAmount + tax).ToString("C")));
-            result.Append("</body></html>");
-            return result.ToString();
-        }
 
     }
 }
